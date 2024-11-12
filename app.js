186647785b33bcef4ae0de9cc1a4f35b2f846c96
Satyam Kumar <satyamk653@gmail.com>
2024-11-12 20:16:21 +0530

    add initial files

--- app.js
+++ app.js
@@ -10,19 +10,20 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-
+const dbUrl = process.env.ATLASDB_URL;
 
 main().then(() => {
     console.log("connected to DB");
@@ -31,7 +32,7 @@ main().then(() => {
 })
 
 async function main() { 
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine","ejs");
@@ -40,7 +41,21 @@ app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
+
+const store =  MongoStore.create({
+    mongoUrl : dbUrl,
+    crypto : {
+        secret : process.env.SECRET,
+    },
+    touchAfter : 24*3600,
+});
+
+store.on("error",() => {
+    console.log("ERROR in MONGO SESSION STORE",err)
+})
+
 const sessionOptions = {
+    store,
     secret : process.env.SECRET,
     resave : false,
     saveUninitialized : true,
@@ -51,12 +66,16 @@ const sessionOptions = {
     }
 }
 
-app.get("/",(req,res) => {
-    res.send("root is working");
-}); 
+// app.get("/",(req,res) => {
+//     res.send("root is working");
+// }); 
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+
 
 app.use(passport.initialize());
 app.use(passport.session());
